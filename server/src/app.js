882b2917b0887a90845502635a07065e7b9b9621
Guy Kibby <GuyKibby@gmail.com>
2022-06-03 15:52:55 +1200

    GET /restaurants endpoint added

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const RestaurantModel = require("./models/RestaurantModel");
+// const formatRestaurant = require("./utils/formatReservation");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await RestaurantModel.find({});
+  return res.status(200).send(restaurants);
+});
 
 module.exports = app;
