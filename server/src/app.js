4a3f6b6b578bd1886f1f714f6179ed7c1efa7215
Guy Kibby <GuyKibby@gmail.com>
2022-06-04 10:50:45 +1200

    GET/restaurants:id endpoint added

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const RestaurantModel = require("./models/RestaurantModel");
 const formatRestaurant = require("./utils/formatRestaurant");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -14,6 +15,24 @@ app.get("/restaurants", async (req, res, next) => {
     return res.status(200).send(restaurants.map(formatRestaurant));
   } catch (e) {
     next(e);
+  }
+});
+
+app.get("/restaurants/:id", async (request, response) => {
+  const { id } = request.params;
+  const isIdValid = mongoose.Types.ObjectId.isValid(id);
+  if (isIdValid) {
+    const restaurant = await RestaurantModel.findById(id);
+
+    if (restaurant) {
+      return response.send(formatRestaurant(restaurant));
+    } else {
+      return response.status(404).send({
+        message: "The restaurant trying to be retrieved does not exist",
+      });
+    }
+  } else {
+    return response.status(400).send({ message: "Invalid ID is provided" });
   }
 });
 
